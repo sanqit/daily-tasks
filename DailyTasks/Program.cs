a09d6537fc9d6c6d22e185e2b05e655cc00bb8eb
sanqit <admin@sanqit.ru>
2024-07-07 23:11:32 +0300

    added first daily task

---
+++ DailyTasks/Program.cs
@@ -0,0 +1,6 @@
+﻿using DailyTasks.Tasks;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Task1.Run();
---
+++ DailyTasks/Tasks/Task1.cs
@@ -0,0 +1,33 @@
+﻿namespace DailyTasks.Tasks;
+
+public class Task1
+{
+    public static void Run()
+    {
+        Console.WriteLine("Введите число, программа проверит является ли число четно красивым");
+
+        var input = Console.ReadLine()!;
+
+        string? resultMessage = default;
+        foreach (var inputChar in input)
+        {
+            if (!char.IsDigit(inputChar))
+            {
+                resultMessage = "Не является числом";
+                break;
+            }
+
+            var digit = inputChar - '0';
+
+            if (digit % 2 != 0)
+            {
+                resultMessage = "Число не является четнокрасивым";
+                break;
+            }
+        }
+
+        resultMessage ??= "Число является четно красивым";
+
+        Console.WriteLine(resultMessage);
+    }
+}
